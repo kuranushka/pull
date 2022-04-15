5da6e59e6e0ab0f3e9a020959514f039cc7ff648
Andrey Kuranov <kuranov.andrey@gmail.com>
2022-04-15 12:51:56 +0300

    Functional Ready

--- src/main/java/ru/kuranov/pull/config/SecurityConfig.java
+++ src/main/java/ru/kuranov/pull/config/SecurityConfig.java
@@ -1,21 +1,15 @@
 package ru.kuranov.pull.config;
 
 import lombok.RequiredArgsConstructor;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.configurers.ExpressionUrlAuthorizationConfigurer;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 import ru.kuranov.pull.service.UserService;
-
-import javax.sql.DataSource;
 
 
 @Configuration
@@ -37,11 +31,8 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         );
 
         http.authorizeRequests((requests) -> ((ExpressionUrlAuthorizationConfigurer.AuthorizedUrl) requests.anyRequest()).authenticated());
-        http
-                .formLogin();
-        http
-                .logout();
-
+        http.formLogin();
+        http.logout();
         http.httpBasic();
         http.csrf().disable();
     }
