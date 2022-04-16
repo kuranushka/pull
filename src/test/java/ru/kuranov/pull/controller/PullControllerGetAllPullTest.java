d599d14fdad1a7031a9c0279c8a7b7f759da78ef
Andrey Kuranov <kuranov.andrey@gmail.com>
2022-04-16 18:36:27 +0300

    Get Pull tests - ready

--- src/test/java/ru/kuranov/pull/controller/PullControllerGetAllPullTest.java
+++ src/test/java/ru/kuranov/pull/controller/PullControllerGetAllPullTest.java
@@ -37,25 +37,25 @@ class PullControllerGetAllPullTest {
 
                 .andExpect(jsonPath("$[0].items").value(hasSize(3)))
 
-                .andExpect(jsonPath("$[0].id").value("2"))
+                .andExpect(jsonPath("$[0].id").value(2))
                 .andExpect(jsonPath("$[0].beginDate").value("01.01.2022"))
                 .andExpect(jsonPath("$[0].endDate").value("31.12.2022"))
                 .andExpect(jsonPath("$[0].description").value("Активный опрос"))
                 .andExpect(jsonPath("$[0].isActive").value("true"))
 
-                .andExpect(jsonPath("$[0].items[0].id").value("4"))
+                .andExpect(jsonPath("$[0].items[0].id").value(4))
                 .andExpect(jsonPath("$[0].items[0].question").value("Сколько часовых поясов В России?"))
                 .andExpect(jsonPath("$[0].items[0].type").value("SIMPLE_STRING"))
                 .andExpect(jsonPath("$[0].items[0].answer", hasEntry(is("11 часовых поясов"), is(false))))
 
-                .andExpect(jsonPath("$[0].items[1].id").value("5"))
+                .andExpect(jsonPath("$[0].items[1].id").value(5))
                 .andExpect(jsonPath("$[0].items[1].question").value("Столица Татарстана?"))
                 .andExpect(jsonPath("$[0].items[1].type").value("SINGLE_OPTION"))
                 .andExpect(jsonPath("$[0].items[1].answer", hasEntry(is("Набережные Челны"), is(false))))
                 .andExpect(jsonPath("$[0].items[1].answer", hasEntry(is("Казань"), is(true))))
                 .andExpect(jsonPath("$[0].items[1].answer", hasEntry(is("Уфа"), is(false))))
 
-                .andExpect(jsonPath("$[0].items[2].id").value("6"))
+                .andExpect(jsonPath("$[0].items[2].id").value(6))
                 .andExpect(jsonPath("$[0].items[2].question").value("Выберите летние виды спорта"))
                 .andExpect(jsonPath("$[0].items[2].type").value("MULTI_OPTION"))
                 .andExpect(jsonPath("$[0].items[2].answer", hasEntry(is("Лыжные гонки"), is(false))))
@@ -75,23 +75,23 @@ class PullControllerGetAllPullTest {
 
                 .andExpect(jsonPath("$[0].items").value(hasSize(3)))
 
-                .andExpect(jsonPath("$[0].id").value("1"))
+                .andExpect(jsonPath("$[0].id").value(1))
                 .andExpect(jsonPath("$[0].beginDate").value("01.01.2022"))
                 .andExpect(jsonPath("$[0].endDate").value("31.12.2022"))
                 .andExpect(jsonPath("$[0].description").value("Не активный опрос"))
                 .andExpect(jsonPath("$[0].isActive").value("false"))
 
-                .andExpect(jsonPath("$[0].items[0].id").value("1"))
+                .andExpect(jsonPath("$[0].items[0].id").value(1))
                 .andExpect(jsonPath("$[0].items[0].question").value("Сколько часов в сутках?"))
                 .andExpect(jsonPath("$[0].items[0].type").value("SIMPLE_STRING"))
                 .andExpect(jsonPath("$[0].items[0].answer").isEmpty())
 
-                .andExpect(jsonPath("$[0].items[1].id").value("2"))
+                .andExpect(jsonPath("$[0].items[1].id").value(2))
                 .andExpect(jsonPath("$[0].items[1].question").value("Столица Татарстана?"))
                 .andExpect(jsonPath("$[0].items[1].type").value("SINGLE_OPTION"))
                 .andExpect(jsonPath("$[0].items[1].answer").isEmpty())
 
-                .andExpect(jsonPath("$[0].items[2].id").value("3"))
+                .andExpect(jsonPath("$[0].items[2].id").value(3))
                 .andExpect(jsonPath("$[0].items[2].question").value("Выберите зимние виды спорта?"))
                 .andExpect(jsonPath("$[0].items[2].type").value("MULTI_OPTION"))
                 .andExpect(jsonPath("$[0].items[2].answer").isEmpty())
@@ -99,25 +99,25 @@ class PullControllerGetAllPullTest {
 
                 .andExpect(jsonPath("$[1].items").value(hasSize(3)))
 
-                .andExpect(jsonPath("$[1].id").value("2"))
+                .andExpect(jsonPath("$[1].id").value(2))
                 .andExpect(jsonPath("$[1].beginDate").value("01.01.2022"))
                 .andExpect(jsonPath("$[1].endDate").value("31.12.2022"))
                 .andExpect(jsonPath("$[1].description").value("Активный опрос"))
                 .andExpect(jsonPath("$[1].isActive").value("true"))
 
-                .andExpect(jsonPath("$[1].items[0].id").value("4"))
+                .andExpect(jsonPath("$[1].items[0].id").value(4))
                 .andExpect(jsonPath("$[1].items[0].question").value("Сколько часовых поясов В России?"))
                 .andExpect(jsonPath("$[1].items[0].type").value("SIMPLE_STRING"))
                 .andExpect(jsonPath("$[1].items[0].answer", hasEntry(is("11 часовых поясов"), is(false))))
 
-                .andExpect(jsonPath("$[1].items[1].id").value("5"))
+                .andExpect(jsonPath("$[1].items[1].id").value(5))
                 .andExpect(jsonPath("$[1].items[1].question").value("Столица Татарстана?"))
                 .andExpect(jsonPath("$[1].items[1].type").value("SINGLE_OPTION"))
                 .andExpect(jsonPath("$[1].items[1].answer", hasEntry(is("Набережные Челны"), is(false))))
                 .andExpect(jsonPath("$[1].items[1].answer", hasEntry(is("Казань"), is(true))))
                 .andExpect(jsonPath("$[1].items[1].answer", hasEntry(is("Уфа"), is(false))))
 
-                .andExpect(jsonPath("$[1].items[2].id").value("6"))
+                .andExpect(jsonPath("$[1].items[2].id").value(6))
                 .andExpect(jsonPath("$[1].items[2].question").value("Выберите летние виды спорта"))
                 .andExpect(jsonPath("$[1].items[2].type").value("MULTI_OPTION"))
                 .andExpect(jsonPath("$[1].items[2].answer", hasEntry(is("Лыжные гонки"), is(false))))
